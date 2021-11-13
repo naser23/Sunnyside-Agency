a4b3ccbdc6784ff53d0097c3c9ccacc33ed4f4c9
Naser Abdulkadir <naserabdulkadir0@gmail.com>
2021-11-12 19:49:00 -0500

    added the base functionality for the hamburger button. will finish the styling of the popup menu for all mobile devices and try to get a javascript transition on it tomorrow.

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 "use strict";
+// global variables //
+const hamburgerButton = document.querySelector(".hamburger-menu-button");
+const hamburgerIcon = document.querySelector(".hamburger-menu-icon");
+console.log(hamburgerIcon);
+const creativesArea = document.querySelector(".creatives-area");
+const popupMenu = document.querySelector(".popup-menu-area");
+let isOpen = false;
+
+hamburgerButton.addEventListener("click", toggleHamburgerMenu);
+
+function toggleHamburgerMenu() {
+  if (isOpen == false) {
+    creativesArea.style.display = "none";
+    popupMenu.style.display = "flex";
+    isOpen = true;
+  } else if (isOpen == true) {
+    creativesArea.style.display = "flex";
+    popupMenu.style.display = "none";
+    isOpen = false;
+  }
+}
